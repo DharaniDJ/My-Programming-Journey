7e4b38c94808d29a675183cb6dad5898dc36b4e8
DharaniDJ <dharani56525@gmail.com>
2023-05-25 15:56:09 -0700

    problem5: InvertBinaryTree

---
+++ Grind169/week1/InvertBinaryTree.cpp
@@ -0,0 +1,36 @@
+
+// Problem link: https://leetcode.com/problems/invert-binary-tree/
+
+// Recursion - O(n) time and O(n) space complexity
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(!root) return NULL;
+        auto temp = root->left;
+        root->left = invertTree(root->right);
+        root->right = invertTree(temp);
+        return root;
+    }
+};
+
+
+// Iterative - O(n) time and O(1) space complexity
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(!root) return NULL;
+        
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){
+            TreeNode* f = q.front();
+            q.pop();
+            TreeNode* temp = f->left;
+            f->left=f->right;
+            f->right=temp;
+            if(f->left) q.push(f->left);
+            if(f->right) q.push(f->right);
+        }
+        return root;
+    }
+};
