239276805952988b9f298d9033a8f4edcd1a85c2
DharaniDJ <dharani56525@gmail.com>
2023-05-26 18:26:13 -0700

    problem21: Diameter of BT

---
+++ Grind169/week1/DiameterOfBT.cpp
@@ -0,0 +1,44 @@
+
+// Problem Link: https://leetcode.com/problems/diameter-of-binary-tree/
+
+// Approach1 - O(n) time and O(n) space complexity
+// class Solution {
+// public:
+//     int solve(TreeNode* root, int& ans){
+//         if(!root){
+//             return 0;
+//         }
+//         int lt = solve(root->left,ans);
+//         int rt = solve(root->right,ans);
+        
+//         ans = max(ans,(lt+rt+1));
+//         return max(lt,rt)+1;
+//     }
+//     int diameterOfBinaryTree(TreeNode* root) {
+//         int ans = -1;
+//         solve(root,ans);
+//         return ans-1;
+//     }
+// };
+
+// DP on Trees - O(n) time and O(n) space complexity
+class Solution {
+public:
+    int solve(TreeNode* root, int& ans){
+        if(!root){
+            return 0;
+        }
+        int lt = solve(root->left,ans);
+        int rt = solve(root->right,ans);
+
+        int temp = max(lt,rt)+1;
+        ans = max({ans,temp,lt+rt+1});
+
+        return temp;
+    }
+    int diameterOfBinaryTree(TreeNode* root) {
+        int ans = -1;
+        solve(root,ans);
+        return ans-1;
+    }
+};
