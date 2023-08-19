2cbb6c329911e5f4f17e2357110681b7cc5b1d21
DharaniDJ <dharani56525@gmail.com>
2023-08-18 23:13:51 -0700

    chore : modified valid parentheses

--- Data Structures and Algorithms/Grind169/week1/ValidParentheses.cpp
+++ Data Structures and Algorithms/Grind169/week1/ValidParentheses.cpp
@@ -1,34 +1,53 @@
 
 // Problem link: https://leetcode.com/problems/valid-parentheses/
 
-// Brute force - O(n^2) time and O(1) space complexity
+// Approach 1 with vector - O(n) time and O(n) space complexity
 // class Solution {
 // public:
-//     vector<int> twoSum(vector<int>& nums, int target) {
-//         int n = nums.size();
-//         for(int i=0;i<(n-1);i++){
-//             for(int j=i+1;j<n;j++){
-//                 if(nums[i]+nums[j]-target == 0) return {i,j};
+//     bool isValid(string str) {
+//         int n = str.size();
+//         unordered_map<char,char> mp;
+//         vector<char> st;
+//         mp['(']=')';
+//         mp['{']='}';
+//         mp['[']=']';
+//         for(char c:str){
+//             if(mp.count(c)) st.push_back(c);
+//             else{
+//                 if(st.size()==0) return false;
+//                 else{
+//                     char tp = st.back();
+//                     if(mp[tp]==c) st.pop_back();
+//                     else return false;
+//                 }
 //             }
 //         }
-//         return {};
+//         return (st.size()==0);
 //     }
 // };
 
 
-// One-pass Hash Table - O(n) time and O(n) space complexity
+// Approach 2 with stack - O(n) time and O(n) space complexity
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int,int> mp;
-        int n = nums.size();
-        for(int i=0;i<n;i++){
-            int k = target-nums[i];
-            if(mp.find(k)!=mp.end()){
-                return {mp[k],i};
+    bool isValid(string str) {
+        int n = str.size();
+        unordered_map<char,char> mp;
+        stack<char> st;
+        mp['(']=')';
+        mp['{']='}';
+        mp['[']=']';
+        for(char c:str){
+            if(mp.count(c)) st.push(c);
+            else{
+                if(st.empty()) return false;
+                else{
+                    char tp = st.top();
+                    if(mp[tp]==c) st.pop();
+                    else return false;
+                }
             }
-            mp[nums[i]]=i;
         }
-        return {};
+        return st.empty();
     }
 };
